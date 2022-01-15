5b082079d1e48f3687d4121d5ca77bf129d79627
Ana Boldeanu <ana_maria.boldeanu@stud.acs.upb.ro>
2022-01-15 19:10:02 +0200

    Update Tests & Checker

--- src/checker/Checker.java
+++ src/checker/Checker.java
@@ -8,6 +8,21 @@ import java.io.IOException;
 
 public class Checker {
 
+    private static final int TEST_NUMBER = 30;
+    private static final int SMALL_TEST_START = 1;
+    private static final int SMALL_TEST_END = 12;
+    private static final int SMALL_TEST_SCORE = 1;
+    private static final int MEDIUM_TEST_START = 13;
+    private static final int MEDIUM_TEST_END = 19;
+    private static final int MEDIUM_TEST_SCORE = 2;
+    private static final int LARGE_TEST_START = 20;
+    private static final int LARGE_TEST_END = 29;
+    private static final int LARGE_TEST_SCORE = 3;
+    private static final int FINAL_TEST_SCORE = 4;
+    protected Checker() {
+
+    }
+
     /**
      * This method is used to calculate total score of the implementation and checkstyle
      */
@@ -32,7 +47,7 @@ public class Checker {
      */
     private static void calculateScoreAllTests() {
         int totalScore = 0;
-        for (int i = 1; i <= 19; i++) {
+        for (int i = 1; i <= TEST_NUMBER; i++) {
             totalScore += calculateScore(i);
         }
         System.out.println("-----------------------------------------------------");
@@ -47,12 +62,13 @@ public class Checker {
      * @param testNumber
      *          the testNumber you want to calculate score for
      * @return
-     *          the score of that test (2 for test : 1) (3 for tests : 2 - 15)
-     *          (4 for tests : 16 - 19)
+     *          the score of that test (1 for tests : 1 -12 )
+     *          (2 for tests : 13 - 19) (3 for tests : 20 - 29) (4 for test : 30)
      */
     public static int calculateScore(final Integer testNumber) {
         if (checkOutput(testNumber)) {
-            System.out.println("test" + testNumber + ".json ----------------------------- PASSED (+"
+            System.out.println("test" + testNumber
+                    + ".json ----------------------------- PASSED (+"
                     + getScoreForTest(testNumber) + ")");
             return getScoreForTest(testNumber);
         } else {
@@ -74,10 +90,8 @@ public class Checker {
         ObjectMapper mapper = new ObjectMapper();
 
         try {
-            JsonNode output = mapper.readTree(new File("output/out_"
-                    + testNumber + ".json"));
-            JsonNode ref = mapper.readTree(new File("ref/ref_test"
-                    + testNumber + ".json"));
+            JsonNode output = mapper.readTree(new File("output/out_" + testNumber + ".json"));
+            JsonNode ref = mapper.readTree(new File("ref/ref_test" + testNumber + ".json"));
 
             return output.equals(ref);
 
@@ -92,12 +106,20 @@ public class Checker {
      * @param testNumber
      *      the testNumber you want to calculate score for
      * @return
-     *      the score of that test (2 for test : 1) (3 for tests : 2 - 15) (4 for tests : 16 - 19)
+     *      the score of that test (1 for tests : 1 -12 )
+     *      (2 for tests : 13 - 19) (3 for tests : 20 - 29) (4 for test : 30)
      */
     private static int getScoreForTest(final Integer testNumber) {
-        if (testNumber == 1) {
-            return 2;
+
+        if (testNumber >= SMALL_TEST_START && testNumber <= SMALL_TEST_END) {
+            return SMALL_TEST_SCORE;
         }
-        return (testNumber > 1 && testNumber <= 15) ? 3 : 4;
+        if (testNumber >= MEDIUM_TEST_START && testNumber <= MEDIUM_TEST_END) {
+            return MEDIUM_TEST_SCORE;
+        }
+        if (testNumber >= LARGE_TEST_START && testNumber <= LARGE_TEST_END) {
+            return LARGE_TEST_SCORE;
+        }
+        return FINAL_TEST_SCORE;
     }
 }
